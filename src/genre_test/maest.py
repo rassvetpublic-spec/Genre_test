ddfa109a536455bbb9e14de8df0a80b612c78465
rassvetpublic <rassvetpublic@gmail.com>
2026-08-23 23:54:56 +0300

    v0.3.3: satisfy Ruff import rule

--- src/genre_test/maest.py
+++ src/genre_test/maest.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 import os
+from collections.abc import Iterable
 from dataclasses import dataclass, field
-from typing import Iterable
 
 from .runtime_meta import default_hf_home
 
